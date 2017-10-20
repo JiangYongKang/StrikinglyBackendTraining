40b4f17e1caee059161d4cfab943cdddf985a83f
JiangYongKang <vincent4502237@gmail.com>
2017-10-21 00:10:33 +0800

    matches routes.

--- web-framework-sinatra-demo/application.rb
+++ web-framework-sinatra-demo/application.rb
@@ -1,6 +1,21 @@
 require 'sinatra'
 
+# TODO: matches Get localhost:4567
 get '/' do
-  @message = 'Hello sinatra'
-  erb :default_index
+  'Hello sinatra'
+end
+
+# TODO: matches Get localhost:4567/vincent
+get '/:name' do
+  params['name']
+end
+
+# TODO: matches Get localhost:4567/say/hello/to/world
+get '/say/*/to/*' do
+  params['splat'].to_s
+end
+
+# TODO: matches Get localhost:4567/download/rubydoc.pdf
+get '/download/*.*' do
+  params['splat'].to_s
 end
