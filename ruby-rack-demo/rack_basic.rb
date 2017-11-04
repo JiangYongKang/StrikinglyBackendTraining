2e1c3e0b8c5175b1ebf535880202c8f5a6c22eea
JiangYongKang <vincent4502237@gmail.com>
2017-11-04 22:52:40 +0800

    ruby rack

--- ruby-rack-demo/rack_basic.rb
+++ ruby-rack-demo/rack_basic.rb
@@ -1,7 +1,8 @@
+# 基本的 rack 程序
 require 'rack'
 
 application = proc do |env|
   ['200', { 'Content-Type' => 'text/html' }, ['Hello, Rack!']]
 end
 
-Rack::Handler::WEBrick.run(application, :Port => 8080, :Host => '0.0.0.0')
+Rack::Handler::WEBrick.run(application, :Port => 8080, :Host => 'localhost')
